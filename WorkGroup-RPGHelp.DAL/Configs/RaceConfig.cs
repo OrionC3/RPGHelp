b30ced2438f7cf6d4d4d781809b18e4c265f156e
OrionC3 <93842749+OrionC3@users.noreply.github.com>
2025-10-15 16:49:13 +0200

    Work Race, BonusRacial

---
+++ WorkGroup-RPGHelp.DAL/Configs/RaceConfig.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkGroup_RPGHelp.DL.Entities;
+
+namespace WorkGroup_RPGHelp.DAL.Configs
+{
+    public class RaceConfig : IEntityTypeConfiguration<Race>
+    {
+        public void Configure(EntityTypeBuilder<Race> builder)
+        {
+            builder.ToTable("Race").HasKey(r => r.Id);
+            builder.Property(r => r.Id).ValueGeneratedOnAdd();
+
+            builder.HasIndex(r => r.Name).IsUnique();
+            builder.Property(r => r.Name).IsRequired().HasMaxLength(255);
+
+            builder.HasOne(r => r.BonusRacial)
+                   .WithOne(b => b.Race)
+                   .HasForeignKey<Race>(j => j.BonusRacialId);
+
+            builder.HasData(
+                new Race { Id = 1, Name = "Elf", BonusRacialId = 1 },
+                new Race { Id = 2, Name = "Orc", BonusRacialId = 2 },
+                new Race { Id = 3, Name = "Half Elf", BonusRacialId = 3 }
+            );
+        }
+    }
+}
---
+++ WorkGroup-RPGHelp.DL/Entities/Race.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkGroup_RPGHelp.DL.Entities
+{
+    public class Race
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TravalSpeed { get; set; }
+        public BonusRacial BonusRacial { get; set; }
+        public int BonusRacialId { get; set; }
+    }
+}
